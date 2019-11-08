7500349638cdac2e5f88248c3188ea71d540b5a9
MaciekTalaska <maciek.talaska@gmail.com>
2019-11-08 23:04:40 +0100

    cleanup

--- scripts/category_ribbon.js
+++ scripts/category_ribbon.js
@@ -2,27 +2,9 @@
 (function() {
   var categoryRibbonFilter, colorRegex, escape;
 
-  //console.log('hexo: ', hexo);
-  //console.log('hexo util: ', hexo.util);
-
-  //var util = require('hexo-util');
-  //console.log('util', util);
-  //console.log('util.escape: ', util.escape);
-
-  //escape = hexo.util.escape;
-  //escape = util.escapeRexExp;
-
   colorRegex = /^\s*(?:\#[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|\#[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|rgba?\(.*\)|hsla?\(.*\))\s*$/;
 
   categoryRibbonFilter = function(data, callback) {
-    console.log('-- -- -- --')
-    //console.log('data:', data.categories)
-    //console.log('data.categories[0]', data.categories[0])
-    //console.log('data.cateories.name: ', data.categories.name)
-    console.log('* * * * * * * * * * *')
-    //console.log('xxx:', data.categories.data[0].name )
-    //console.log('link: ', data.categories.data[0].permalink)
-    console.log('-- -- -- --')
     var categoryConfig, ref, ribbon;
     if (data.categories == null) {
       //return callback();
@@ -44,15 +26,11 @@
 
     if (typeof data.ribbon === 'string') {
       data.ribbon = {
-        //text: data.ribbon
-        //text: data.categories.data[0].name
         text: t
       };
     }
     ribbon = data.ribbon;
     if (ribbon.text == null) {
-      //ribbon.text = data.categories[0];
-      //ribbon.link = "/categories/" + (ribbon.text, hexo.config.filename_case) + "/";
       ribbon.text = t
       ribbon.link = l
     }
@@ -71,10 +49,6 @@
     } else {
       console.log("Unknown Category: " + ribbon.text);
     }
-//    if (callback) {
-//      return callback(null, data);
-//    }
-    //return data.categories.name;
   };
 
   hexo.extend.filter.register('before_post_render', categoryRibbonFilter);
